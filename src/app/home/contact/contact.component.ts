67af36a1573025e2fd141d291007df8fac735733
Semjasa <Marceldezanna@yahoo.de>
2024-11-22 23:01:25 +0100

    env, gitignore, service

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../../../services/contact.service';
 
 @Component({
-  selector: 'contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
-  standalone: true
+    selector: 'contact',
+    imports: [],
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.scss',
+    standalone: true
 })
+
 export class ContactComponent {
+
+    constructor( private contactService: ContactService ) {
+
+
+    }
 
 }
