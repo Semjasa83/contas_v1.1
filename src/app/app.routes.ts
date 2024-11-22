95e5a466ca3607052153bb08531ce3ec6357ae42
Semjasa <Marceldezanna@yahoo.de>
2024-11-22 19:35:24 +0100

    home route and repush

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
-  { path: '',   redirectTo: 'login', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }, // redirect to `first-component`
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   // { path: '**', component: PageNotFoundComponent },
 ];
