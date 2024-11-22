2a652b4488941f823cbcd675f084fb410f0b844a
Semjasa <Marceldezanna@yahoo.de>
2024-11-22 19:45:36 +0100

    navigation raw implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from '../utilities/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NavbarComponent
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  standalone: true
 })
 export class HomeComponent {
 
