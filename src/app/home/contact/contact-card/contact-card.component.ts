299a92a6aa02c03ea03a47f5c645d16ae1a8c8df
Semjasa <Marceldezanna@yahoo.de>
2024-11-26 00:16:02 +0100

    pass data to dialog

--- src/app/home/contact/contact-card/contact-card.component.ts
+++ src/app/home/contact/contact-card/contact-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { Contact } from "../../../interfaces/contact.interface";
 
 @Component({
@@ -8,7 +8,18 @@ import { Contact } from "../../../interfaces/contact.interface";
     styleUrl: './contact-card.component.scss'
 })
 export class ContactCardComponent {
+
     @Output('showDialog') showDialog = new EventEmitter<boolean>();
+    //@Input('data') data: Contact = {};
+    @Input() contactData?: Contact;
+
+    constructor() {
+
+    }
+
+    public ngOnInit() {
+        console.log(this.contactData);
+    }
 
     public preventPropagation(event: any) {
         event.stopPropagation();
--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ContactService } from '../../../services/contact.service';
 import { Contact } from "../../interfaces/contact.interface";
 import { ContactCardComponent } from "./contact-card/contact-card.component";
@@ -17,23 +17,24 @@ export class ContactComponent {
 
     public contacts: Contact[] = [];
     public showContactDialog: boolean = false;
+    public selectedContact?: Contact;
 
-    constructor( private contactService: ContactService ) {
-
-    }
+    constructor( private contactService: ContactService) {}
 
     public async ngOnInit() {
         this.contactService.getAllContacts();
         this.contactService.contacts$.subscribe(response => {
             this.contacts = response;
-            console.log(this.contacts);
         });
-
+        console.log(this.contacts);
     }
 
     public handleDialog(event: any) {
         this.showContactDialog = event;
-        console.log('Dialog closed:', event);
     }
 
+    public selectContact(contact: Contact) {
+        this.selectedContact = contact;
+        this.showContactDialog = true;
+    }
 }
